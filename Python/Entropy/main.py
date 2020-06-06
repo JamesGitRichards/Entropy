d2b93807b8b048bdaf1b4e756223150b6d7f7db1
James <jamesgitrichards@mail.ru>
2020-06-06 03:48:59 +0300

    Done a half

--- Python/Entropy/main.py
+++ Python/Entropy/main.py
@@ -1,6 +1,8 @@
 
-text = open("Python/Entropy/1984.txt", "r").read()
+from math import log2
 
+#text = open("Python/Entropy/1984.txt", "r").read()
+text = "aaaaaaaaasdasvasasassa sadsa dsadasda a"
 text_lenght = len(text)
 
 text_lenght_1 = (text_lenght / 2) + (text_lenght % 2)
@@ -15,26 +17,30 @@ alph_b_lenght = 0
 array_a = []
 array_b = []
 
+probability_array = []
+
 def getIndex(symbol, position):
     global alphabetta_a, alphabetta_b
     aplhabetta = ""
+    
     if(position == True):
         aplhabetta = alphabetta_a
     else:
-        alphabetta = alphabetta_b
-    
+        aplhabetta = alphabetta_b
+        
+
     for parser in range(0, len(aplhabetta)):
         t = ""
         t = aplhabetta[parser]
 
         if symbol == t:
             return parser
-    
+    #print("N2 = " + str(alphabetta_b))
     return -1
 
 def setAplhabetta(txt, position, lenght):
     global alphabetta_a, alphabetta_b
-    begin = 0
+    begin = 1
     if position == True:
         begin = 0
     else:
@@ -43,36 +49,73 @@ def setAplhabetta(txt, position, lenght):
     for iterator in range(begin, lenght, 2):
         index = 0
         index = getIndex(text[iterator], position)
+        ####
+        #print(index)
         if(index == -1):
             if(position == True):
                 alphabetta_a += text[iterator]
             else:
                 alphabetta_b += text[iterator]
+    #print("N2 = " + str(alph_b_lenght))
+    print("Done")
     return True
 
-def setProbability(txt):
-    global alph_a_lenght, alph_b_lenght, array_a, array_b
+def setProbability(txt, prob_arr,arr_a, arr_b):
+    global alph_a_lenght, alph_b_lenght
 
     alph_a_lenght += len(alphabetta_a)
     alph_b_lenght += len(alphabetta_b)
+    #print("N2 = " + str(alph_b_lenght))
 
     for iterator in range(0, alph_a_lenght):
-        array_a.append(0)
+        arr_a.append(0)
 
     for iterator in range(0, alph_b_lenght):
-        array_b.append(0)
+        arr_b.append(0)
 
     for parser in range(0, text_lenght):
         if parser % 2 == 0:
             index = getIndex(text[parser], True)
             if(index >= 0):
-                array_a[index] = array_a[index] + 1
+                arr_a[index] +=  1
         else:
             index = getIndex(text[parser], False)
             if(index >= 0):
-                array_b[index] = array_b[index] + 1
+                arr_b[index] += 1
+    
+    #prob
+    for row in range(alph_a_lenght):
+        prob_arr.append([])
+        for column in range(alph_b_lenght):
+            prob_arr[row].append(0)
+    
+    current_row    = 0
+    current_column = 0
+    for iterator in range(0, (text_lenght - 1), 2):
+        current_row    = getIndex(text[iterator], True)
+        current_column = getIndex(text[iterator + 1], False)
+        prob_arr[current_row][current_column] += 1
+        print(prob_arr[current_row][current_column])
 
-result_true  = setAplhabetta(text, True, text_lenght)
+def Entropy():
+    SchennonEntropyFirst = 0
+    prob = 0
+    for iterator in range(alph_a_lenght):
+        prob = array_a[iterator] / text_lenght_1
+        SchennonEntropyFirst += (-1) * prob * log2(prob)
+    
+    print("test")
+    SchennonEntropySecond = 0
+    for iterator in range(alph_b_lenght):
+        prob = array_b[iterator] / text_lenght_2
+        print(array_b[iterator])
+        SchennonEntropySecond += (-1) * prob * log2(prob)
+    print("end")
+    SchennonEntropyTotal = SchennonEntropyFirst + SchennonEntropySecond
+
+    print("H (thirdparty source) = " + str(SchennonEntropyTotal))
+
+result_true  = setAplhabetta(text, True,  text_lenght)
 result_false = setAplhabetta(text, False, text_lenght)
 
 if result_false == True:
@@ -80,4 +123,8 @@ if result_false == True:
 else:
     print("setAlphabetta False...[SOME ERROR HAS OCCURED]")
 
-setProbability(text)
+print(text_lenght)
+setProbability(text, probability_array, array_a, array_b)
+print(array_a)
+print(array_b)
+Entropy()
